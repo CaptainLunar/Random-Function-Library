3c6fc6cfd7bc7711d6d2a1c7daadaf56b0e5e880
CaptainLunar <jeanmalmonte@yahoo.com>
2016-03-17 09:23:02 -0500

    Update binaryAddFunc.hs

--- binaryAddFunc.hs
+++ binaryAddFunc.hs
@@ -8,6 +8,6 @@ import Numeric (showIntAtBase)
 bl :: String -> String -> IO ()
 bl x y = b(d(x), d(y))
     where d = foldl' (\c x -> c * 2 + digitToInt x) 0 
-          b (x, y) = if y == 0
-            then putStrLn $ showIntAtBase 2 intToDigit x ""
-            else b(xor x y, shiftL (x.&.y) 1) 
+          b (x, y)
+            | y == 0    = putStrLn $ showIntAtBase 2 intToDigit x ""
+            | otherwise = b(xor x y, shiftL (x.&.y) 1) 
